39b5e528867e50455bab24c5b13f6c339eea5605
Hyunje  park <hjp1nku@korea.ac.kr>
2025-02-15 13:37:28 +0900

    ..

--- Sewformer/env_constants.py
+++ Sewformer/env_constants.py
@@ -17,6 +17,7 @@ elif hostname == "server" : # 4090 X 4
     SEWFORMER_PROJ_ROOT  = "/media/hjp/db6095ca-a560-4c3a-90ad-b667ec189671/REFERENCES/SewFormerAnalysis"
     # DATASET_ROOT    = "/media/hjp/05aba9a7-0e74-4e54-9bc9-5f11b9c4c757/sewfactory"
     DATASET_ROOT    = "/media/hjp/05aba9a7-0e74-4e54-9bc9-5f11b9c4c757/SewFactory/sewfactory"
+    SEWING_DATA_ROOT = "/media/hjp/05aba9a7-0e74-4e54-9bc9-5f11b9c4c757/3D_GARMENT_SEWING_GEN"
 
 elif hostname == "hjp-MS-7D42" : # 3090 X 1
     DATASET_ROOT    = "/media/hjp/efef19d3-9b92-453c-ba04-c205f7233cab/VTO_DATASET/PoC59"
@@ -25,5 +26,6 @@ elif hostname == "gpu-1" : # H100 X 8
     PYGARMENT_ROOT  = "/data/hjp/VTO2025/GarmentCodeAnalysis"
     SEWFORMER_PROJ_ROOT  = "/data/hjp/VTO2025/REFERENCES/sewformer"
     DATASET_ROOT    = "/data/hjp/VTO2025/DATASETs/SewFactory"
+    SEWING_DATA_ROOT = "/data/hjp/VTO2025/DATASETs/SewingPattern"
     
 WANDB_USERNAME = ""
