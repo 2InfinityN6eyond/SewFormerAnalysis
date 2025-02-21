7048cda7c28b0f52156fbca41c72d951ee6fe547
Hyunje Park <hjp1nskku@g.skku.edu>
2025-02-21 12:50:39 +0900

    ..

---
+++ Sewformer/ANALYSIS/analysis_pose.py
@@ -0,0 +1,9 @@
+# USAGE
+# /Applications/Autodesk/maya2022/Maya.app/Contents/bin/mayapy analysis_pose.py
+
+from maya import cmds
+import pymel.core as pm
+
+# Other dependencies
+import numpy as np
+import json
